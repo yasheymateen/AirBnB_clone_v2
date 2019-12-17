09bb28c7b38f3764fffa23f5c93791ed31f0fecc
Marco Chan <inspiredtolive0@gmail.com>
2019-12-17 15:07:32 -0800

    Adds a public instance method to delete objects and filtering to all()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,11 +20,17 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """returns a dictionary
         Return:
             returns a dictionary of __object
         """
+        if cls:
+            filtered = {}
+            for k, v in self.__objects.items():
+                if k.split('.')[0] == cls.__name__:
+                    filtered[k] = v
+            return filtered
         return self.__objects
 
     def new(self, obj):
@@ -55,3 +61,11 @@ class FileStorage:
                     self.__objects[key] = value
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """Deletes objects in __objects."""
+        if obj:
+            objs_to_delete = self.all(obj.__class__)
+            for k in objs_to_delete:
+                del self.__objects[k]
+            self.save()
