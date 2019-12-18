61cf633ada3ea54890ad8aeebeb8cef9bd09ec55
Marco Chan <inspiredtolive0@gmail.com>
2019-12-18 02:11:59 -0800

    Initializes dates if missing

--- models/base_model.py
+++ models/base_model.py
@@ -44,6 +44,11 @@ class BaseModel:
                     setattr(self, key, value)
             if self.id is None:
                 setattr(self, 'id', str(uuid.uuid4()))
+            now = datetime.now()
+            if self.created_at is None:
+                self.created_at = now
+            if self.updated_at is None:
+                self.updated_at = now
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
